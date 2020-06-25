7ec981028db33ce83eaaee4435905dc4559d6472
AppVeyor <ci@appveyor.com>
2020-06-25 11:10:41 +0000

    53605a9

---
+++ precache-manifest.534d1c55cbbbd3602f91ebf8d8a4c720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921a00a96c7ce4f4e3e655731524b6f8",
+    "url": "/ra-homeworks-router-task2/index.html"
+  },
+  {
+    "revision": "1305e46dd71fe784b2ae",
+    "url": "/ra-homeworks-router-task2/static/css/main.fe503a5d.chunk.css"
+  },
+  {
+    "revision": "6c3d40e4700c4ff7c91b",
+    "url": "/ra-homeworks-router-task2/static/js/2.fad9c5ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ra-homeworks-router-task2/static/js/2.fad9c5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1305e46dd71fe784b2ae",
+    "url": "/ra-homeworks-router-task2/static/js/main.d4f6b786.chunk.js"
+  },
+  {
+    "revision": "081c88385b8775bac3bb",
+    "url": "/ra-homeworks-router-task2/static/js/runtime-main.3e40f19d.js"
+  },
+  {
+    "revision": "fabe1c14d8f364ada6513cc088d99a9c",
+    "url": "/ra-homeworks-router-task2/static/media/loader.fabe1c14.gif"
+  }
+]);
